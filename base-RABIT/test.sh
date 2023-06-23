3207c8f1de73d3111c07ab3d0894525a4c0ca153
WangShengyuan <2021010764@secoder.net>
2023-06-23 23:09:25 +0800

    remove irrelevant dirs

--- base-RABIT/test.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES="0" python test.py --input_path ./data/val_A_labels_resized --img_path ../train_resized/imgs --which_epoch 180
--- nima.pytorch/tests/unit/test_emd_loss.py
+++
@@ -1,2 +0,0 @@
-def test_emd_loss():
-    assert 1 == 1
